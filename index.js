76575c3d97b90efd2ca24d7f4ad33c13776be716
bingbingguo <chinaguobingbing@126.com>
2019-03-28 17:46:13 +0800

    ts
    ts

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const config =require('./config')
 const start = require('./start')
 const route = require('./route')
 
-if (config.mode == "dev"){
+if (config.mode === "dev"){
     http.createServer(route.dispatch).listen(config.port,config.host,()=>{
-        console.log('Process'+process.pid+ `is listening in ${config.host}:${config.port} for incoming requests`)
+        console.log('Process: ['+process.pid+ `]is listening in ${config.host}:${config.port} for incoming requests`)
     })
 }else{
     start(route.dispatch,config.host,config.port)
